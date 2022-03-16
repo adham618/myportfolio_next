6e64d5692102dbf2d407ae1c2e22eb47324cc495
adham618 <adhamtarek291@gmail.com>
2022-03-16 19:15:25 +0200

    feat: import header && footer component

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
-import Footer from './Footer';
-import Header from './Header';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   // Put Header or Footer Here
