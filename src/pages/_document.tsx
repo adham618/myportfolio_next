89926851900fd34a84110c1fbe62eda8ba96801e
adham618 <adhamtarek291@gmail.com>
2022-03-16 19:12:54 +0200

    feat: commnt link for fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,13 +16,11 @@ class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
-          <link
-            rel='preload'
-            href='/fonts/inter-var-latin.woff2'
-            as='font'
-            type='font/woff2'
-            crossOrigin='anonymous'
-          />
+          {/* !STARTERCONF add google font only here or remove it */}
+          {/* <link
+            href='https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap'
+            rel='stylesheet'
+          /> */}
         </Head>
         <body>
           <Main />
