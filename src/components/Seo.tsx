05a2a6dbb2ec4b1814d4229e9e8d20ac8348b3cd
adham618 <adhamtarek291@gmail.com>
2022-03-16 19:10:05 +0200

    chore: add site name

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -5,8 +5,8 @@ import { openGraph } from '@/lib/helper';
 
 // !STARTERCONF Change these default meta
 const defaultMeta = {
-  title: 'Next.js + Tailwind CSS + TypeScript Starter',
-  siteName: 'Next.js + Tailwind CSS + TypeScript Starter',
+  title: 'Adham Tarek',
+  siteName: 'Adham Tarek',
   description:
     'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
