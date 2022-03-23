29ab4621111a747ba31bf9662904ce21064d1ff6
adham618 <adhamtarek291@gmail.com>
2022-03-23 16:50:03 +0200

    feat: config nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,30 @@
 import { AppProps } from 'next/app';
+import Router from 'next/router';
 import { ThemeProvider } from 'next-themes';
+import NProgress from 'nprogress';
+import { useEffect } from 'react';
 
+import '@/styles/nprogress.css';
 import '@/styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const delay = 500; // in milliseconds
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    let timer: any;
+    const load = () => {
+      timer = setTimeout(function () {
+        NProgress.start();
+      }, delay);
+    };
+    const stop = () => {
+      clearTimeout(timer);
+      NProgress.done();
+    };
+    Router.events.on('routeChangeStart', () => load());
+    Router.events.on('routeChangeComplete', () => stop());
+    Router.events.on('routeChangeError', () => stop());
+  }, []);
   return (
     <ThemeProvider attribute='class' defaultTheme='system'>
       <Component {...pageProps} />
