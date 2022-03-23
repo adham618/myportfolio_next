24c96c2e3a9221031f52ee67bc94afc11b24caad
adham618 <adhamtarek291@gmail.com>
2022-03-23 16:51:46 +0200

    feat: add message to console

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 
+import sayHello from '@/lib/sayHello';
+
 import Footer from './Footer';
 import Header from './Header';
-
 export default function Layout({ children }: { children: React.ReactNode }) {
-  // Put Header or Footer Here
+  sayHello();
   return (
     <>
       <Header />
