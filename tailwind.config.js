759e0a245600fdfb7b5cf2739c986da1c8fcc303
adham618 <adhamtarek291@gmail.com>
2022-03-24 21:02:48 +0200

    feat: change color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,3 @@
-function withOpacityValue(variable) {
-  return ({ opacityValue }) => {
-    if (opacityValue === undefined) {
-      return `rgb(var(${variable}))`;
-    }
-    return `rgb(var(${variable}) / ${opacityValue})`;
-  };
-}
 /* eslint-disable @typescript-eslint/no-var-requires */
 module.exports = {
   content: [
@@ -14,64 +6,18 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
-    // add fonts here
-    //   fontFamily: {
-    //     sans: ['Roboto'],
-    //     mono: ['Fira Code'],
-    //     serif: ['Playfair Display'],
-    //   },
     extend: {
       colors: {
-        primary: {
-          // Customize it on globals.css :root
-          50: withOpacityValue('--tw-color-primary-50'),
-          100: withOpacityValue('--tw-color-primary-100'),
-          200: withOpacityValue('--tw-color-primary-200'),
-          300: withOpacityValue('--tw-color-primary-300'),
-          400: withOpacityValue('--tw-color-primary-400'),
-          500: withOpacityValue('--tw-color-primary-500'),
-          600: withOpacityValue('--tw-color-primary-600'),
-          700: withOpacityValue('--tw-color-primary-700'),
-          800: withOpacityValue('--tw-color-primary-800'),
-          900: withOpacityValue('--tw-color-primary-900'),
-        },
-        dark: '#1E293B',
-        content: 'slate-500',
-        contentdark: 'slate-400',
-        bgcontent: '#1E293B',
-      },
-      keyframes: {
-        flicker: {
-          '0%, 19.999%, 22%, 62.999%, 64%, 64.999%, 70%, 100%': {
-            opacity: 0.99,
-            filter:
-              'drop-shadow(0 0 1px rgba(252, 211, 77)) drop-shadow(0 0 15px rgba(245, 158, 11)) drop-shadow(0 0 1px rgba(252, 211, 77))',
-          },
-          '20%, 21.999%, 63%, 63.999%, 65%, 69.999%': {
-            opacity: 0.4,
-            filter: 'none',
-          },
-        },
-        shimmer: {
-          '0%': {
-            backgroundPosition: '-700px 0',
-          },
-          '100%': {
-            backgroundPosition: '700px 0',
-          },
-        },
-      },
-      animation: {
-        flicker: 'flicker 3s linear infinite',
-        shimmer: 'shimmer 1.3s linear infinite',
+        primary: '#19141C',
+        'primary-focus': '#140d16',
+        'primary-content': '#B3C5EF',
+        dark: '#16121A',
+        content: '#334155',
+        'content-dark': '#94a3b8',
+        bgcontent: '#1A161F',
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    '@tailwindcss/typography',
-    require('daisyui'),
-  ],
   daisyui: {
     styled: true,
     themes: [],
@@ -82,4 +28,9 @@ module.exports = {
     prefix: '',
     //darkTheme: 'dark',
   },
+  plugins: [
+    require('@tailwindcss/forms'),
+    '@tailwindcss/typography',
+    require('daisyui'),
+  ],
 };
