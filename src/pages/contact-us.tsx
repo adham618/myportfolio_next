a8a680cb4dab1d539775c17722fd322d2c2397a6
adham618 <adhamtarek291@gmail.com>
2022-03-24 21:06:11 +0200

    create contact us page

---
+++ src/pages/contact-us.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+import Layout from '@/components/layout/Layout';
+import Seo from '@/components/Seo';
+
+export default function ContactUSPage() {
+  return (
+    <Layout>
+      <Seo templateTitle='Contact US' />
+
+      <main>
+        <section className='dark:bg-dark'>
+          <div className='layout dark:text-contentdark min-h-screen py-20 text-content'>
+            Contact-US
+          </div>
+        </section>
+      </main>
+    </Layout>
+  );
+}
