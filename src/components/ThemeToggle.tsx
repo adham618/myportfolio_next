3912dc64150c48584c96652a3d373977bf2c1c48
adham618 <adhamtarek291@gmail.com>
2022-03-25 21:17:06 +0200

    bug: fix bug prop d doesn't exit in server

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,7 +1,15 @@
 import { useTheme } from 'next-themes';
-import { BsFillMoonStarsFill, BsFillSunFill } from 'react-icons/bs';
+import { useEffect, useState } from 'react';
+import { BsFillSunFill, BsMoonStarsFill } from 'react-icons/bs';
+
+export const useLoaded = () => {
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => setLoaded(true), []);
+  return loaded;
+};
 
 export default function ThemeToggle() {
+  const loaded = useLoaded();
   const { theme, setTheme } = useTheme();
   return (
     <button
@@ -9,8 +17,8 @@ export default function ThemeToggle() {
       className='p-2 hover:rounded-full hover:bg-slate-200 focus:outline-none  dark:text-slate-400 dark:hover:bg-opacity-40'
       onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
     >
-      {theme === 'light' ? (
-        <BsFillMoonStarsFill size={15} />
+      {theme === 'light' && loaded ? (
+        <BsMoonStarsFill size={18} />
       ) : (
         <BsFillSunFill size={18} />
       )}
