1d7c86b54fa42a08605824553d5858767f9144e9
adham618 <adhamtarek291@gmail.com>
2022-03-25 14:57:39 +0200

    feat: fix deploy

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,47 +1,65 @@
+// import * as React from 'react';
+
+// import Layout from '@/components/layout/Layout';
+// import Seo from '@/components/Seo';
+// interface AboutPageProps {
+//   data: {
+//     attributes: {
+//       title: string;
+//       content: string;
+//     };
+//   };
+// }
+// export default function AboutlPage({ data }: AboutPageProps) {
+//   return (
+//     <Layout>
+//       <Seo templateTitle='About' />
+
+//       <main>
+//         <section className='dark:bg-dark'>
+//           <div className='dark:text-contentdark layout min-h-screen py-20 text-content'>
+//             <h1>{data.attributes.title}</h1>
+//             <>
+//               {JSON.stringify(data.attributes.content).replace(
+//                 /\s|&nbsp;/g,
+//                 ''
+//               )}
+//             </>
+//           </div>
+//         </section>
+//       </main>
+//     </Layout>
+//   );
+// }
+// // export const getStaticProps = async () => {
+// //   const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL;
+// //   const res = await fetch(`${NEXT_PUBLIC_API_URL}/api/about`);
+// //   const data = await res.json();
+// //   if (!data) {
+// //     return {
+// //       notFound: true,
+// //     };
+// //   }
+// //   return {
+// //     props: { data: data.data },
+// //     revalidate: 1,
+// //   };
+// // };
 import * as React from 'react';
 
 import Layout from '@/components/layout/Layout';
 import Seo from '@/components/Seo';
-interface AboutPageProps {
-  data: {
-    attributes: {
-      title: string;
-      content: string;
-    };
-  };
-}
-export default function AboutlPage({ data }: AboutPageProps) {
+
+export default function AboutPage() {
   return (
     <Layout>
       <Seo templateTitle='About' />
 
       <main>
-        <section className='dark:bg-dark'>
-          <div className='dark:text-contentdark layout min-h-screen py-20 text-content'>
-            <h1>{data.attributes.title}</h1>
-            <>
-              {JSON.stringify(data.attributes.content).replace(
-                /\s|&nbsp;/g,
-                ''
-              )}
-            </>
-          </div>
+        <section className=''>
+          <div className='layout min-h-screen py-20'></div>
         </section>
       </main>
     </Layout>
   );
 }
-// export const getStaticProps = async () => {
-//   const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL;
-//   const res = await fetch(`${NEXT_PUBLIC_API_URL}/api/about`);
-//   const data = await res.json();
-//   if (!data) {
-//     return {
-//       notFound: true,
-//     };
-//   }
-//   return {
-//     props: { data: data.data },
-//     revalidate: 1,
-//   };
-// };
