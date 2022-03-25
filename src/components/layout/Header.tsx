6e353dcba16500f68f49648162bc3a1a3fc1cbe2
adham618 <adhamtarek291@gmail.com>
2022-03-25 21:16:10 +0200

    style: fix icon size

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -115,9 +115,9 @@ export default function Header() {
             className='ml-3 cursor-pointer p-1.5 hover:rounded-md hover:bg-slate-200 focus:outline-none dark:text-slate-400  dark:hover:bg-opacity-40 md:hidden  '
           >
             {open ? (
-              <IoClose className='inline-block h-6 w-6 stroke-current dark:text-slate-400' />
+              <IoClose className='inline-block h-7 w-7 stroke-current dark:text-slate-400' />
             ) : (
-              <HiOutlineMenu className='inline-block h-6 w-6 stroke-current checked:hidden dark:text-slate-400' />
+              <HiOutlineMenu className='inline-block h-7 w-7 stroke-current checked:hidden dark:text-slate-400' />
             )}
           </button>
         </div>
