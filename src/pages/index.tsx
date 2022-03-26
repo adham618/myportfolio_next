d53968ce14bd90e857149c6c232a46c9d1159c4e
adham618 <adhamtarek291@gmail.com>
2022-03-26 19:34:34 +0200

    chore: import component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,15 @@
 import * as React from 'react';
 
-import Layout from '@/components/layout/Layout';
+import Contact from '@/components/Contact';
+import Intro from '@/components/Intro';
+import Layout from '@/components/Layout';
+import Projects from '@/components/Projects';
 import Seo from '@/components/Seo';
-
-/**
- * SVGR Support
- * Caveat: No React Props Type.
- *
- * You can override the next-env if the type is important to you
- * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
- */
+import Services from '@/components/Services';
 
 // !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
 // Before you begin editing, follow all comments with `STARTERCONF`,
 // to customize the default configuration.
-// Don't forget to add this classes to any HTML rendered from Markdown,
-// or pulled from a CMS. (Typography plugin)
-
-// Class	               Gray scale
-// prose-gray (default)	 Gray
-// prose-slate	         Slate
-// prose-zinc            Zinc
-// prose-neutral	       Neutral
-// prose-stone           Stone
 
 export default function HomePage() {
   return (
@@ -31,11 +18,11 @@ export default function HomePage() {
       <Seo />
       <main>
         <section className='bg-white dark:bg-dark'>
-          <div className='layout flex min-h-screen flex-col items-center justify-center text-center'>
-            {/* Intro
-            Services
-            Projects
-            Contact */}
+          <div className='layout min-h-screen'>
+            <Intro />
+            <Services />
+            <Projects />
+            <Contact />
           </div>
         </section>
       </main>
