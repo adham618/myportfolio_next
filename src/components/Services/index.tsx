377fd493d682969dffb452452513c6205a23994a
adham618 <adhamtarek291@gmail.com>
2022-03-26 19:34:56 +0200

    feat: create services component

---
+++ src/components/Services/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export default function Services() {
+  return (
+    <div className=''>
+      <h2 className='text-stroke dark:text-stroke-black mb-4 text-9xl font-black  leading-[0.7em] text-slate-200 text-transparent '>
+        What we&apos;re offering
+      </h2>
+    </div>
+  );
+}
