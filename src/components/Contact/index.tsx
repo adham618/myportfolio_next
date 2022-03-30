234481b61dcda7d5da38a53edc6c44e306ac9ab5
adham618 <adhamtarek291@gmail.com>
2022-03-30 16:21:02 +0200

    feat: add calendly to contact component

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,50 +1,49 @@
 /* eslint-disable @next/next/no-img-element */
 import * as React from 'react';
 
+import ButtonLink from '../Button/ButtonLink';
+import Calendly from '../Calendly';
+import StrokeTitle from '../StrokeTitle';
+
 export default function Contact() {
   return (
-    <section className='bg-slate-100 dark:bg-primary'>
-      <div className='layout prose prose-slate flex max-h-[60rem] min-h-[30rem] w-full flex-col justify-center py-20'>
-        <div className='flex flex-col items-center justify-center'>
-          <h2 className='mb-4 stroke-black text-9xl font-black leading-[0.7em]  text-slate-200 text-transparent dark:stroke-white '>
-            START YOUR PROJECT WITH{' '}
-            <span className='text-black dark:text-white'>Me</span>
-          </h2>
-          <p className='prose prose-slate '>
-            Tell us more about your project. Send us a message <br />
-            and we&apos;ll get back to you as soon as possible.
+    <section className=' bg-slate-100 dark:bg-primary'>
+      <div className='layout  flex h-[30rem] w-full flex-col justify-center py-80'>
+        <div className='flex flex-col items-center justify-center text-center'>
+          <StrokeTitle
+            title={
+              <>
+                start your project with{' '}
+                <span className='text-black dark:text-white'>me</span>
+              </>
+            }
+          />
+          <p className='dark:opacity-50'>
+            Tell me more about your project. Send me a message <br />
+            and I&apos;ll get back to you as soon as possible.
           </p>
         </div>
-        <div className='flex h-14 w-full flex-col items-center justify-center gap-2 sm:flex-row'>
-          <a
-            className='btn btn-outline xl:btn-lg gap-3'
-            href='mailto:hi@manasky.com'
-          >
-            <img
-              className='h-6 w-6'
-              width='24'
-              height='24'
-              src='/svg/gmail.svg'
-              alt='email'
-            />
-            Send us an Email
-          </a>{' '}
-          <div className='divider sm:divider-vertical opacity-50'>OR</div>{' '}
-          <div className='inline-block'>
-            <button className='btn btn-outline xl:btn-lg gap-3'>
-              <img
-                width='24'
-                height='24'
-                className='h-6 w-6'
-                src='/svg/zoom.svg'
-                alt='zoom'
-              />
-              Schedule Zoom meeting
-            </button>
-          </div>{' '}
-        </div>
-        <div className='prose prose-slate'>
-          <h3>hhhhhhhff yf</h3>
+        <div className='mt-24 flex h-14 w-full flex-col items-center justify-center gap-4 sm:mt-11 sm:flex-row'>
+          <ButtonLink
+            href='/contact'
+            content={
+              <>
+                <img
+                  className='h-6 w-6'
+                  width='24'
+                  height='24'
+                  src='/svg/gmail.svg'
+                  alt='email'
+                />
+                Send me an Email
+              </>
+            }
+            className='flex items-center justify-center gap-3 text-center'
+          />{' '}
+          <div className='opacity-50'>OR</div>{' '}
+          <div className='relative inline-block'>
+            <Calendly />
+          </div>
         </div>
       </div>
     </section>
