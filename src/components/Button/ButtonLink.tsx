053f5192c4d581ea2b2d5eb1f65e209af8efb696
adham618 <adhamtarek291@gmail.com>
2022-03-30 16:23:02 +0200

    feat: create buttnlink component

---
+++ src/components/Button/ButtonLink.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import * as React from 'react';
+
+import clsxm from '@/lib/clsxm';
+
+type indexProps = {
+  content: React.ReactNode;
+} & React.ComponentPropsWithoutRef<'a'>;
+
+export default function ButtonLink({
+  className,
+  content,
+  ...rest
+}: indexProps) {
+  return (
+    <Link href='/contact' {...rest}>
+      <a
+        className={clsxm(
+          'rounded-md border border-black bg-transparent px-5 py-4 transition-colors hover:bg-black hover:text-white dark:border-white dark:hover:bg-white dark:hover:text-black',
+          className
+        )}
+      >
+        {content}
+      </a>
+    </Link>
+  );
+}
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,9 +2,20 @@ import * as React from 'react';
 
 import clsxm from '@/lib/clsxm';
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-type indexProps = {} & React.ComponentPropsWithoutRef<'div'>;
+type indexProps = {
+  content: React.ReactNode;
+} & React.ComponentPropsWithoutRef<'button'>;
 
-export default function index({ className, ...rest }: indexProps) {
-  return <div className={clsxm('', className)} {...rest}></div>;
+export default function index({ className, content, ...rest }: indexProps) {
+  return (
+    <button
+      className={clsxm(
+        'rounded-md border border-black bg-transparent px-5 py-4 transition-colors hover:bg-black hover:text-white dark:border-white dark:hover:bg-white dark:hover:text-black',
+        className
+      )}
+      {...rest}
+    >
+      {content}
+    </button>
+  );
 }
