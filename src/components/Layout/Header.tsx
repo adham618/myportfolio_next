47253f48c2ac7c92e221cba2569e354c40136eaa
adham618 <adhamtarek291@gmail.com>
2022-04-01 21:55:25 +0200

    feat: remove services link

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -11,7 +11,6 @@ import ThemeToggle from '../ThemeToggle';
 const links = [
   { href: '/', label: 'Home' },
   { href: '/about', label: 'About' },
-  { href: '#services', label: 'Services' },
   { href: '/projects', label: 'Projects' },
   { href: '/contact', label: 'Contact' },
 ];
