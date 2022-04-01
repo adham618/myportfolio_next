4a56d3c9f388f1b5160de478a1874773db4c3fd8
adham618 <adhamtarek291@gmail.com>
2022-04-01 17:53:30 +0200

    feat: create card component

--- src/components/Services/Card.tsx
+++ src/components/Services/Card.tsx
@@ -1,5 +1,19 @@
 import * as React from 'react';
 
-export default function Card() {
-  return <div></div>;
+interface CardProps {
+  icon: string;
+  title: string;
+  content: string;
+  titleColor: string;
+}
+export default function Card({ icon, title, content, titleColor }: CardProps) {
+  return (
+    <div className='flex h-72 transform flex-col items-center justify-center gap-4 rounded-xl bg-slate-100 text-center  transition duration-500 hover:scale-95 dark:bg-primary md:w-auto'>
+      <div className={`text-2xl font-bold text-${titleColor}`}>
+        <span className='inline-block text-5xl font-black'>{icon}</span>
+        <h2>{title}</h2>
+      </div>
+      <p className='p-5 text-left opacity-50'>{content}</p>
+    </div>
+  );
 }
