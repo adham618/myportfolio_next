9f1c68ac5c86f10d98681141d0354145f80a893a
adham618 <adhamtarek291@gmail.com>
2022-04-01 17:55:02 +0200

    feat: complete intro component

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -1,5 +1,41 @@
 import * as React from 'react';
+import { BsFillHeartFill } from 'react-icons/bs';
+
+import ButtonLink from '../Button/ButtonLink';
+import StrokeTitle from '../StrokeTitle';
 
 export default function Intro() {
-  return <div></div>;
+  return (
+    <section className='layout gap-y-12 py-20'>
+      <div className='flex flex-col items-center justify-center text-center'>
+        <div className='mb-11 max-w-3xl'>
+          <StrokeTitle
+            title={
+              <>
+                YOUR PARTNER{' '}
+                <span className='font-thin text-black dark:text-white'>—</span>{' '}
+                IN <span className='text-black dark:text-white'>DESIGN</span> &{' '}
+                <span className='text-black dark:text-white'>DEVELOPMENT</span>
+              </>
+            }
+          />
+          <p className='text-sm sm:text-base'>
+            I create{' '}
+            <span className='font-bold text-black dark:text-white'>apps</span>{' '}
+            and websites your{' '}
+            <span className='font-bold text-black dark:text-white'>
+              clients
+            </span>{' '}
+            will{' '}
+            <BsFillHeartFill className='ml-1 inline-block	 animate-bounce text-rose-500' />
+          </p>
+        </div>
+        <ButtonLink
+          className='border-none bg-rose-500 px-12 text-white hover:bg-rose-600 dark:hover:bg-rose-600 dark:hover:text-white'
+          href='#contact'
+          content="Let's Get Started!"
+        />
+      </div>
+    </section>
+  );
 }
