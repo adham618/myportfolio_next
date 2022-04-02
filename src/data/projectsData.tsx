8c66cb5fd71de6496e9e1a8cf5f8f937c8deaaf6
adham618 <adhamtarek291@gmail.com>
2022-04-02 17:25:37 +0200

    feat: create proectsdata file

---
+++ src/data/projectsData.tsx
@@ -0,0 +1,47 @@
+const projectsData = [
+  {
+    id: 0,
+    image: '/images/foodo.png',
+    title: 'Foodo Landing Page',
+    siteLink: 'https://foodo-landing-page-navy.vercel.app/',
+    GithubLink: 'https://github.com/adham618/foodo-landing-page',
+    content:
+      'A Landing Page Usign Next JS && TypeScript && React-Bootstrap For Food Delivery App',
+  },
+  {
+    id: 1,
+    image: '/images/medium.png',
+    title: 'Medium Clone',
+    siteLink: 'http://medium-clone-black.vercel.app/',
+    GithubLink: 'https://github.com/adham618/medium-clone',
+    content:
+      'Medium Clone Using Next JS && TypeScript && Tailwind-CSS && Sanity-CMS.',
+  },
+  {
+    id: 2,
+    image: '/images/insta.png',
+    title: 'Instagram Clone',
+    siteLink: 'http://instagram-clone-ruddy.vercel.app/',
+    GithubLink: 'https://github.com/adham618/instagram-clone',
+    content:
+      'Instagram Clone with nextjs, typescript, tailwind CSS, Firebase v9, NextAuth, Recoil',
+  },
+  {
+    id: 3,
+    image: '/images/hulu.png',
+    title: 'Hulu Website Clone',
+    siteLink: 'https://hulu-clone-with-typescript.vercel.app/',
+    GithubLink: 'https://github.com/adham618/hulu-clone',
+    content:
+      'Hulu Website Clone with nextjs, typescript, tailwind CSS, and API from TheMovieDB',
+  },
+  {
+    id: 4,
+    image: '/images/game.png',
+    title: 'Magic Memory',
+    siteLink: 'https://magic-memory-chi.vercel.app/',
+    GithubLink: 'https://github.com/adham618/magic-memory',
+    content: 'a simple memory game with react js',
+  },
+];
+export default projectsData;
