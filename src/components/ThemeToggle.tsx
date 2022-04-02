a8bb3f43881d865ad01057ad55e25410ce202c6b
adham618 <adhamtarek291@gmail.com>
2022-04-02 12:34:55 +0200

    style: fix style

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -14,13 +14,13 @@ export default function ThemeToggle() {
   return (
     <button
       aria-label='theme toggle'
-      className='p-2 hover:rounded-full hover:bg-slate-200 focus:outline-none  dark:text-slate-400 dark:hover:bg-opacity-40'
+      className='p-2 hover:rounded-full hover:bg-slate-200 focus:outline-none  dark:text-slate-200 dark:hover:bg-opacity-40'
       onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
     >
       {theme === 'light' && loaded ? (
-        <BsMoonStarsFill size={18} />
+        <BsMoonStarsFill size={16} />
       ) : (
-        <BsFillSunFill size={18} />
+        <BsFillSunFill size={16} />
       )}
     </button>
   );
