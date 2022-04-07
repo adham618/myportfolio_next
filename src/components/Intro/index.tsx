1778b8b69a847ac5895be581b05ec4d3e78a0f90
adham618 <adhamtarek291@gmail.com>
2022-04-07 19:28:08 +0200

    style: fix style

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -30,7 +30,7 @@ export default function Intro() {
           </p>
         </div>
         <ButtonLink
-          className='flex items-center justify-center border-0 border-none bg-rose-500 px-12 text-center text-slate-50 hover:bg-rose-600 dark:hover:bg-rose-600'
+          className='flex items-center justify-center border-0 border-none bg-rose-500 px-12 text-center text-slate-50 hover:bg-rose-600 dark:hover:bg-rose-600 dark:hover:text-slate-50'
           href='#contact'
           content="Let's Get Started!"
         />
