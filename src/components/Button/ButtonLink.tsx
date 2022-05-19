66002d6b87f9dc4d6a0fc81b4cbc282cc87c33e8
adham618 <adhamtarek291@gmail.com>
2022-05-19 19:32:10 +0200

    style: fix border

--- src/components/Button/ButtonLink.tsx
+++ src/components/Button/ButtonLink.tsx
@@ -22,6 +22,7 @@ export default function ButtonLink({
           'rounded-md border border-black bg-transparent px-5 py-4 transition-colors hover:bg-black hover:text-white dark:border-white dark:hover:bg-white dark:hover:text-black',
           className
         )}
+        {...rest}
       >
         {content}
       </a>
