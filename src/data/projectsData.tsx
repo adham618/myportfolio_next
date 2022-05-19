b9c658a1d5f1fce560ba90a46ccebef395fb5325
adham618 <adhamtarek291@gmail.com>
2022-05-19 02:03:01 +0200

    feat: edit project data

--- src/data/projectsData.tsx
+++ src/data/projectsData.tsx
@@ -1,24 +1,14 @@
 const projectsData = [
   {
     id: 0,
-    image: '/images/foodo.png',
-    title: 'Foodo Landing Page',
-    siteLink: 'https://foodo-landing-page-navy.vercel.app/',
-    GithubLink: 'https://github.com/adham618/foodo-landing-page',
-    content:
-      'A Landing Page Usign Next JS && TypeScript && React-Bootstrap For Food Delivery App',
+    image: '/images/game.png',
+    title: 'Magic Memory',
+    siteLink: 'https://magic-memory-chi.vercel.app/',
+    GithubLink: 'https://github.com/adham618/magic-memory',
+    content: 'a simple memory game with react js',
   },
   {
     id: 1,
-    image: '/images/medium.png',
-    title: 'Medium Clone',
-    siteLink: 'http://medium-clone-black.vercel.app/',
-    GithubLink: 'https://github.com/adham618/medium-clone',
-    content:
-      'Medium Clone Using Next JS && TypeScript && Tailwind-CSS && Sanity-CMS.',
-  },
-  {
-    id: 2,
     image: '/images/insta.png',
     title: 'Instagram Clone',
     siteLink: 'http://instagram-clone-ruddy.vercel.app/',
@@ -27,7 +17,7 @@ const projectsData = [
       'Instagram Clone with nextjs, typescript, tailwind CSS, Firebase v9, NextAuth, Recoil',
   },
   {
-    id: 3,
+    id: 2,
     image: '/images/hulu.png',
     title: 'Hulu Website Clone',
     siteLink: 'https://hulu-clone-with-typescript.vercel.app/',
@@ -36,20 +26,31 @@ const projectsData = [
       'Hulu Website Clone with nextjs, typescript, tailwind CSS, and API from TheMovieDB',
   },
   {
-    id: 4,
-    image: '/images/game.png',
-    title: 'Magic Memory',
-    siteLink: 'https://magic-memory-chi.vercel.app/',
-    GithubLink: 'https://github.com/adham618/magic-memory',
-    content: 'a simple memory game with react js',
+    id: 3,
+    image: '/images/medium.png',
+    title: 'Medium Clone',
+    siteLink: 'http://medium-clone-black.vercel.app/',
+    GithubLink: 'https://github.com/adham618/medium-clone',
+    content:
+      'Medium Clone Using Next JS && TypeScript && Tailwind-CSS && Sanity-CMS.',
   },
+
   {
-    id: 5,
+    id: 4,
     image: '/images/ezServicesHub.png',
     title: 'ezServicesHub',
     siteLink: 'https://ez-service-hub.vercel.app/',
     GithubLink: 'https://github.com/adham618/ez-service-hub',
     content: 'Figma to HTML for a client on Upwork',
+  },
+  {
+    id: 5,
+    image: '/images/foodo.png',
+    title: 'Foodo Landing Page',
+    siteLink: 'https://foodo-landing-page-navy.vercel.app/',
+    GithubLink: 'https://github.com/adham618/foodo-landing-page',
+    content:
+      'A Landing Page Usign Next JS && TypeScript && React-Bootstrap For Food Delivery App',
   },
 ];
 export default projectsData;
