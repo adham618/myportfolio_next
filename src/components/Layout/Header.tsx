667844c72b3a7020845f017053502922fa11a05f
adham618 <adhamtarek291@gmail.com>
2022-05-19 18:45:40 +0200

    style: remove border

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -121,9 +121,9 @@ export default function Header() {
             className='ml-3 cursor-pointer p-1.5 hover:rounded-md hover:bg-slate-200 focus:outline-none dark:text-slate-200  dark:hover:bg-opacity-40 md:hidden  '
           >
             {open ? (
-              <IoClose className='inline-block h-7 w-7 stroke-current dark:text-slate-200' />
+              <IoClose className='inline-block h-7 w-7 border-none stroke-current dark:text-slate-200' />
             ) : (
-              <HiOutlineMenu className='inline-block h-7 w-7 stroke-current checked:hidden dark:text-slate-200' />
+              <HiOutlineMenu className='inline-block h-7 w-7 border-none stroke-current checked:hidden dark:text-slate-200' />
             )}
           </button>
         </div>
