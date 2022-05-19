441a0b00d88185130ccce20fe5f9c4c455a8ca9e
adham618 <adhamtarek291@gmail.com>
2022-05-19 21:06:03 +0200

    style: fffffffffffffff

--- src/components/Button/ButtonLink.tsx
+++ src/components/Button/ButtonLink.tsx
@@ -16,7 +16,7 @@ export default function ButtonLink({
   ...rest
 }: indexProps) {
   return (
-    <Link href={href} {...rest}>
+    <Link href={href} {...rest} passHref>
       <a
         className={clsxm(
           'rounded-md border border-black bg-transparent px-5 py-4 transition-colors hover:bg-black hover:text-white dark:border-white dark:hover:bg-white dark:hover:text-black',
