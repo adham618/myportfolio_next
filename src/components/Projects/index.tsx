6ab4bf2c72cdc3c14ffe18e5ac3e21dfc6c46967
Adham Tarek <adhamtarek291@gmail.com>
2023-02-28 16:09:14 +0200

    feat: add new projects

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -18,16 +18,19 @@ export default function Projects() {
         </p>
       </div>
       <div className='flex flex-col justify-center pt-20 pb-2'>
-        {projectsData.slice(0, 4).map((project) => (
-          <Project
-            key={project.id}
-            image={project.image}
-            title={project.title}
-            siteLink={project.siteLink}
-            GithubLink={project.GithubLink}
-            content={project.content}
-          />
-        ))}
+        {projectsData
+          .reverse()
+          .slice(0, 4)
+          .map((project) => (
+            <Project
+              key={project.id}
+              image={project.image}
+              title={project.title}
+              siteLink={project.siteLink}
+              GithubLink={project.GithubLink}
+              content={project.content}
+            />
+          ))}
       </div>
       <div className='flex justify-center'>
         <ButtonLink
